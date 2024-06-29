5b573bf02eecc08717ac0898d00145d46ac8d354
Chris <clj@ollure.com>
2024-06-28 23:56:57 -0500

    setting app.js up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ( BrowserRouter as Router, Route, Routes ) from 'react-router-dom';
+import 'boostrap/dist/css/boostrap.min.css';
+import NavBar from './Components/NavBar';
+import AddHabit from './Components/AddHabit';
+import ViewWeekly from './Components/ViewWeekly';
 
-function App() {
+const App = () => (
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/" element={<AddHabit />} />
+          <Route path="/" elemet ={<ViewWeekly />} />
+        </Routes>
+      </div>
+    </>
+    );
   );
-}
 
 export default App;
